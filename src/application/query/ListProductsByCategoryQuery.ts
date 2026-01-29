f1b67cde3e10af9aac5fbb08d36c20d8fc0d8909
iang-ts <ian.dev.empresarial@gmail.com>
2026-01-29 17:14:27 -0400

    list products by category

---
+++ src/application/query/ListProductsByCategoryQuery.ts
@@ -0,0 +1,52 @@
+import { QueryCommand } from '@aws-sdk/lib-dynamodb';
+import { dynamoClient } from '@infra/clients/dynamoClient';
+import { ProductItem } from '@infra/database/dynamo/items/ProductItem';
+import { Injectable } from '@kernel/decorators/Injectable';
+import { AppConfig } from '@shared/config/AppConfig';
+
+@Injectable()
+export class ListProductsByCategoryQuery {
+  constructor(private readonly appConfig: AppConfig) {}
+
+  async execute(category: string): Promise<ListProductsByCategoryQuery.Output[]> {
+    const command = new QueryCommand({
+      TableName: this.appConfig.db.dynamodb.mainTable,
+      IndexName: 'GSI2',
+      ProjectionExpression: '#GSI2PK, #id, #name, #description, #price, #category, #inputFileKey, #stockQuantity, #createdAt',
+      KeyConditionExpression: '#GSI2PK = :GSI2PK',
+      ScanIndexForward: false,
+      ExpressionAttributeNames: {
+        '#GSI2PK': 'GSI2PK',
+        '#id': 'id',
+        '#name': 'name',
+        '#description': 'description',
+        '#price': 'price',
+        '#category': 'category',
+        '#inputFileKey': 'inputFileKey',
+        '#stockQuantity': 'stockQuantity',
+        '#createdAt': 'createdAt',
+      },
+      ExpressionAttributeValues: {
+        ':GSI2PK': ProductItem.getGSI2PK(category),
+      },
+    });
+
+    const { Items: productsItem = [] } = await dynamoClient.send(command);
+
+    return productsItem.map(product => ProductItem.toEntity(product as ProductItem.ItemType));
+  }
+}
+
+export namespace ListProductsByCategoryQuery {
+  export type Output = {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    category: string;
+    inputFileKey: string;
+    stockQuantity: number;
+    isActive: boolean;
+    createdAt: Date;
+  }
+}
